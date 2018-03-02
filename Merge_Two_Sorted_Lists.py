60b44b9319f80a920f54a5c08472ebd0f82209f4
David <david@FlatMagic.home>
2018-03-01 21:09:23 -0500

    initial commit

--- Merge_Two_Sorted_Lists.py
+++ Merge_Two_Sorted_Lists.py
@@ -1,10 +1,4 @@
-fdsajkfldsafdjksal
-fds
-afd
-safd
-safd
-saf
-dsaf
-dsa
-fdsaf
-ds
+def merge_two_sorted_lists:
+	"""Given two sorted lists such as l1 = [1, 2, 5] and l2 = [1, 3, 4, 5, 7] combine them into one solution_list = [1, 1, 2, 3, 4, 5, 5, 7]"""
+	
+	def
