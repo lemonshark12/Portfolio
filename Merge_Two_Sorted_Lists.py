76cb5c7314137496fe346754d4c3ba0b99c71ce1
David <david@FlatMagic.home>
2018-03-01 21:28:18 -0500

    challenge part #1 complete

--- Merge_Two_Sorted_Lists.py
+++ Merge_Two_Sorted_Lists.py
@@ -1,4 +1,11 @@
-def merge_two_sorted_lists:
+import itertools
+
+def merge_two_sorted_lists():
 	"""Given two sorted lists such as l1 = [1, 2, 5] and l2 = [1, 3, 4, 5, 7] combine them into one solution_list = [1, 1, 2, 3, 4, 5, 5, 7]"""
 	
-	def
+	l1 = [1 ,2, 5]
+	l2 = [1, 3, 4, 5, 7]
+	l3 = sorted(l1 + l2)
+	print (l3)
+	
+merge_two_sorted_lists()
