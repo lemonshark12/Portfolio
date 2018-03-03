d20fe1f22c7300b1fd60c7a9372923c4621035d8
David <david@FlatMagic.home>
2018-03-03 14:40:46 -0500

    extended code to calculate digits before decimal point, the “.” and the next digit after

--- Division without division.py
+++ Division without division.py
@@ -1,44 +1,68 @@
 from itertools import count, islice
 
+n = 24
+m = 7
+
 def solution():
 	"""divide two integers without using multiplication, division, or mod operator; if overflow, return MAX_INT"""
+	digits_and_remainder = calculate_digits(n, m)
+	print (digits_and_remainder)
+	calculate_remainder(digits_and_remainder)
+
+def calculate_digits(n, m):
 	
-#	try:
-	n = 24
-	m = 7
 	r = list(range(n))
 	ans = m
 	rem = m
-		#short = list
-	#	(islice(r, 0, m))
 	division_list = []
 	while ans < n:
-		
-			#print (ans)
-			#count = 0
 		ans += m
 		division_list.append(ans)
-			#count += 1
-	print (division_list)
-	digits = str(len(division_list))
+#	print (division_list)
+	digits = len(division_list)
+	last = division_list[-1]
+	remainder = (n - digits*m)*10
+	return (digits, remainder)
 	
-	remainder = str(division_list.pop())
-	
+def calculate_remainder(digits_and_remainder):
+	remainders_list = []
 	remainder_list = []
+	digits, rem = int(digits_and_remainder[0]), digits_and_remainder[1]
+#	print (digits, rem)
+#	digits = 3, rem = 30
 	
-#	if remainder != 0:
-	n = int(remainder)
-	print (n, m, ans, rem)
-	while rem < n:
-		rem += m
-		remainder_list.append(rem)
-		print (n, m, ans, rem)
-	print (remainder_list)
+	remainder = m
+#	remainder = 7
+	while rem != 0:
+		while remainder < rem:
+#	while 7 < 30
+			remainder += m
+# 	add 7 to the remainder variable
+			remainder_list.append(remainder)
+#	attach the remainder variable to the remainder_list
+#		print (n, m, remainder, rem)
+			print (remainder_list)
 	
-	remainder = str(len(remainder_list))
-		
-	print (remainder)	
-	print (digits + "." + remainder)
+		remainders = len(remainder_list)
+		last = remainder_list[-1]
+		rem = (rem - remainders*m)*10
+		decimals = remainders_list.append(remainders)
+		remainder_list = []
+
+	print (last)
+	print (remainder)
+	print (decimals)
+	#print (new_remainder)
+	
+	#while remainders != 0:
+	
+	#	print (decimals)
+				
+	#return (remainders)	
+	
+	
+	
+#	print (str(digits) + "." + str(remainders_list.join()))
 	balls = 24/7
 	print (balls)
 	
